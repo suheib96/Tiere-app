3b8007288e14647c2ade0ae83222abb951dc5e07
Suheib Marzouka <suheibmarzouka@MBP-von-Suheib.fritz.box>
2024-07-24 10:26:57 +0200

    Routen hinzugefügt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import './App.css';
+import AnimalList from './components/AnimalList';
+import EditAnimal from './components/EditAnimal';
+import AddAnimal from './components/AddAnimal';
 
 function App() {
   return (
-    <h1>hallo</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AnimalList />}/> 
+        <Route path="/list" element={<AnimalList />}/> 
+        <Route path="/editAnimal" element={<EditAnimal />}/> 
+        <Route path="/addAnimal" element={<AddAnimal />}/> 
+
+      </Routes>
+
+
+    </Router>
   );
 }
 
